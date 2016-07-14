a827b93f749c5fd907f046c7b2ea3357442a59c0
Daniel Knight <daniel.knight@retrofi.co.uk>
2016-07-14 22:26:30 +0100

    fixed issue where local variable was declared in the wrong place

--- consolidated-lights.lua
+++ consolidated-lights.lua
@@ -2,85 +2,88 @@
 %% properties
 64 value
 57 value
-98 value
+69 value
 %% globals
 --]]
 
-table = {
-  ["57"] = {
-    ["roomName"] = "Master Bedroom",
-    devices = {223, 225},
-    morning = {
+-- This is where you place definitions for the script
+definitions = {
+  ["64"] = {
+    roomName = "Landing",
+    lights = {62},
+    Morning = {
       ["onCmd"] = nil,
       ["offCmd"] = nil},
-    day = {
+    Day = {
       ["onCmd"] = nil,
       ["offCmd"] = nil},
-    evening = {
+    Evening = {
       ["onCmd"] = 75,
       ["offCmd"] = 0},
-    night = {
+    Night = {
+      ["onCmd"] = 10,
+      ["offCmd"] = 0},
+    },
+  ["57"] = {
+    roomName = "Master Bedroom",
+    lights = {51, 55},
+    Morning = {
+      ["onCmd"] = nil,
+      ["offCmd"] = nil},
+    Day = {
+      ["onCmd"] = nil,
+      ["offCmd"] = nil},
+    Evening = {
+      ["onCmd"] = 75,
+      ["offCmd"] = nil},
+    Night = {
       ["onCmd"] = nil,
       ["offCmd"] = nil},
     },
-
-  ["64"] = {
-    ["roomName"] = "Landing",
-    devices = {178},
-    ["morning"] = {
-      onCmd = 20,
-      offCmd = 0},
-    ["day"] = {
-      onCmd = nil,
-      offCmd = nil},
-    ["evening"] = {
-      onCmd = 75,
-      offCmd = 0},
-    ["night"] = {
-      onCmd = 10,
-      offCmd = 0},
+  ["69"] = {
+    roomName = "Bathroom",
+    lights = {74},
+    Morning = {
+      ["onCmd"] = nil,
+      ["offCmd"] = nil},
+    Day = {
+      ["onCmd"] = nil,
+      ["offCmd"] = nil},
+    Evening = {
+      ["onCmd"] = 75,
+      ["offCmd"] = 0},
+    Night = {
+      ["onCmd"] = 10,
+      ["offCmd"] = 0},
     },
-  ["98"] = {
-    ["roomName"] = "Kitchen",
-    devices = {178},
-    ["morning"] = {
-      onCmd = 20,
-      offCmd = 0},
-    ["day"] = {
-      onCmd = nil,
-      offCmd = nil},
-    ["evening"] = {
-      onCmd = 75,
-      offCmd = 0},
-    ["night"] = {
-      onCmd = 10,
-      offCmd = 0},
-    }
-
 }
 
-
+-- get the thing that triggered the scene to run
 local trigger = fibaro:getSourceTrigger()
 local triggerDevice = tostring(trigger["deviceID"])
-print(triggerDevice)
 
+-- check to make sure the scene was triggered by a device, if not, abort
 if triggerDevice == "nil" then
-  print("Scene was not triggered by a device")
-else
+  print("Scene was not triggered by a device, aborting...")
+  fibaro:abort()
+end
 
-  local motionStatus = tonumber(fibaro:getValue(triggerDevice, "value"))
-  local houseMode = fibaro:getGlobal("houseMode")
+-- get the value of the triggering motion sensor, store in variable
+local motionStatus = tonumber(fibaro:getValue(triggerDevice, "value"))
+-- get the current house mode from global variable
+local houseMode = tostring(fibaro:getGlobal("houseMode"))
+-- declare variable for later
+local lightValue
 
-  if motionStatus == 1 then
-    print("Motion detected in the " .. table[triggerDevice].roomName)
-    local deviceValue = table[triggerDevice][houseMode].onCmd
-  elseif motionStatus == 0 then
-    print("Motion no longer detected in the " .. table[triggerDevice].roomName)
-    local deviceValue = table[triggerDevice][houseMode].offCmd
-  end
+if motionStatus == 1 then
+  --if there is motion, set the light value to the onCmd value
+  lightValue = definitions[triggerDevice][houseMode].onCmd
+elseif motionStatus == 0 then
+  --if there is no motion, set the light value to the offCmd value
+  lightValue = definitions[triggerDevice][houseMode].offCmd
+end
 
-  for key, value in pairs(table[triggerDevice].devices) do
-    print("Changing device " .. value .. " to " .. deviceValue)
-  end
-
+--for each light in the 'lights' array, set it to the 'lightValue'
+for key, value in pairs(definitions[triggerDevice].lights) do
+  fibaro:call(value, "setValue", lightValue)
 end
